87922c2a4554c54a2c3259a1f5f3729f7c9779f9
zek9618 <zeljko.kljajic090503@gmail.com>
2025-11-13 11:37:00 +0100

    testat 3: displaying and sending is not functioning yet

---
+++ main.js
@@ -0,0 +1,6 @@
+// Datenbereich
+window.backendUrl = "https://online-lectures-cs.thi.de/chat/e22aa467-5671-4de9-9c68-64c141a7f4e5/friend";
+
+//Token
+window.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiVG9tIiwiaWF0IjoxNzYzMDIzMTk4fQ.AE5odz77S0DxjRc-ff1WTc24UNpwlH2UMmeicsV-i2U";
+//Jerry: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiSmVycnkiLCJpYXQiOjE3NjMwMjMxOTh9.B32GKLEN4nA6V7opNpcLuxZnR9H6o539bB83YUF-mfw"
